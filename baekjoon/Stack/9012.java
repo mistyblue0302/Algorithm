8f05106aab48815debbea640b3ec51d4af83c49b
이주선 <96303344+dilmah0203@users.noreply.github.com>
2023-03-14 20:46:39 +0900

    Create 9012.java

---
+++ baekjoon/Stack/9012.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t; i++) {
+            sb.append(solve(br.readLine())).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static String solve(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                return "NO";
+            } else {
+                stack.pop();
+            }
+        }
+
+        //검사를 마치고 스택의 잔여 요소 검사
+        if (stack.isEmpty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+}
