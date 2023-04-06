df2dc369e0b724cd004d1fec83c07721b954e9dd
이주선 <96303344+dilmah0203@users.noreply.github.com>
2023-04-07 02:22:11 +0900

    Update 11053.java

--- baekjoon/DP/11053.java
+++ baekjoon/DP/11053.java
@@ -4,19 +4,18 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
 
-    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
 
-        int n = Integer.parseInt(br.readLine()); //수열의 크기 1<=n<=1000
-        StringTokenizer st;
-
-        int array[] = new int[n];
-        int dp[] = new int[n]; //n번째 수열일 때 가장 긴 증가하는 부분 수열 길이
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            array[i] = Integer.parseInt(st.nextToken());
+            int temp = Integer.parseInt(st.nextToken());
+            arr[i] = temp;
         }
 
         dp[0] = 1;
@@ -25,12 +24,13 @@ public class Main {
         for (int i = 1; i < n; i++) {
             dp[i] = 1;
             for (int j = 0; j < i; j++) {
-                if(array[i] > array[j] && dp[i] <= dp[j]) {
+                if (arr[i] > arr[j] && dp[i] <= dp[j]) {
                     dp[i] = dp[j] + 1;
                 }
             }
             result = Math.max(result, dp[i]);
         }
+
         System.out.println(result);
     }
 }
