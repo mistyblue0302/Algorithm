42351dc5440b176ddc0f1a6b5cad2d9540edde81
이주선 <96303344+dilmah0203@users.noreply.github.com>
2023-04-07 02:32:32 +0900

    Update 11053.java

--- baekjoon/DP/11053.java
+++ baekjoon/DP/11053.java
@@ -1,36 +1,41 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int[] array = new int[n];
         int[] dp = new int[n];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         for (int i = 0; i < n; i++) {
-            int temp = Integer.parseInt(st.nextToken());
-            arr[i] = temp;
+            array[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp[0] = 1;
-        int result = 0;
-
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
+            // dp의 값 1로 초기화
             dp[i] = 1;
             for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j] && dp[i] <= dp[j]) {
+                // arr를 돌며 i가 j의 값보다 크고 이전 dp의 크기보다 작으면
+                if (array[j] < array[i] && dp[i] <= dp[j]) {
                     dp[i] = dp[j] + 1;
                 }
             }
-            result = Math.max(result, dp[i]);
         }
 
+        int result = -1;
+        for (int i = 0; i < n; i++) {
+            if(dp[i] > result) {
+                result = dp[i];
+            }
+        }
         System.out.println(result);
     }
 }
