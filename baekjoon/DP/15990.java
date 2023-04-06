7a360a31374f0e673f76971227a832a8f9c876d6
이주선 <96303344+dilmah0203@users.noreply.github.com>
2023-04-06 21:47:41 +0900

    Create 15990.java

---
+++ baekjoon/DP/15990.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[100001][4];
+
+        dp[1][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for (int i = 4; i <= 100000; i++) {
+            dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % 1000000009;
+            dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % 1000000009;
+            dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % 1000000009;
+        }
+
+        long result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int k = Integer.parseInt(br.readLine());
+            result = (dp[k][1] + dp[k][2] + dp[k][3]) % 1000000009;
+            System.out.println(result);
+        }
+    }
+}
