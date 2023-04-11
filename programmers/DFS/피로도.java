3a13bd112c764d200b2dd3c4e604d1a67ec5778c
이주선 <96303344+dilmah0203@users.noreply.github.com>
2023-04-12 07:13:29 +0900

    Update 피로도.java

--- programmers/DFS/피로도.java
+++ programmers/DFS/피로도.java
@@ -1,24 +1,21 @@
 class Solution {
     static int answer = 0;
-    static boolean[] visit;
+    static boolean[] visited;
 
     public int solution(int k, int[][] dungeons) {
-        visit = new boolean[dungeons.length];
-
+        visited = new boolean[dungeons.length];
         dfs(0, k, dungeons);
-
         return answer;
     }
 
     public void dfs(int depth, int k, int[][] dungeons) {
-        for (int i = 0; i < dungeons.length; i++) {
-            if (!visit[i] && dungeons[i][0] <= k) {
-                visit[i] = true;
-                dfs(depth + 1, k - dungeons[i][1], dungeons);
-                visit[i] = false;
+        for(int i =0;i<dungeons.length;i++){
+            if(k >=dungeons[i][0] && !visited[i]) {
+                visited[i] = true;
+                dfs(depth+1, k-dungeons[i][1], dungeons);
+                visited[i] = false;
             }
         }
-
-        answer = Math.max(answer, depth);
+       answer = Math.max(answer, depth);
     }
 }
