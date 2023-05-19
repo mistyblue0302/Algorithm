9ab018708e64d5fe1745f03f72dd5eb63a522a1e
이주선 <96303344+dilmah0203@users.noreply.github.com>
2023-05-19 15:56:10 +0900

    Update 1759.java

--- baekjoon/DFS/1759.java
+++ baekjoon/DFS/1759.java
@@ -16,7 +16,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         L = Integer.parseInt(st.nextToken()); //4
