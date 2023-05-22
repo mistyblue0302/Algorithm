653341b1513e2f96d765f9f7fa26ddf4700302fb
이주선 <96303344+dilmah0203@users.noreply.github.com>
2023-05-23 01:10:49 +0900

    Update 1918.java

--- baekjoon/Stack/1918.java
+++ baekjoon/Stack/1918.java
@@ -1,50 +1,62 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-
-    public static int priority(char ch) {
-        if (ch == '(') return 0;
-        if (ch == '+' || ch == '-') return 1;
-        else return 2;
-    }
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        String s = br.readLine();
+        String str = br.readLine();
 
-        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
-                bw.write(s.charAt(i));
-            } else if (s.charAt(i) == '(') {
-                stack.push(s.charAt(i));
-            } else if (s.charAt(i) == ')') {
-                while (!stack.isEmpty()) {
-                    if (stack.peek() == '(') {
-                        stack.pop();
+        for (int i = 0; i < str.length(); i++) {
+            char now = str.charAt(i);
+
+            switch (now){
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    while (!stack.isEmpty() && priority(stack.peek()) >= priority(now)) {
+                        sb.append(stack.pop());
                     }
-                    bw.write(stack.pop());
-                }
-            } else {
-                while (!stack.isEmpty() && priority(stack.peek()) >= priority(s.charAt(i))) {
-                    // 스택이 비어 있지 않고 스택 상단 값의 우선순위가 입력 문자보다 크면
-                    // 괄호안이 아니고 stack 최상단 값이 * 나 /면
-                    bw.write(stack.pop());
-                    // 스택 상단 값 출력
-                }
-                stack.add(s.charAt(i));
-                // 다시 담는다
+                    stack.add(now);
+                    break;
+                case '(':
+                    stack.add(now);
+                    break;
+                case ')':
+                    while(!stack.isEmpty() && stack.peek() != '('){
+                        sb.append(stack.pop());
+                    }
+                    stack.pop();
+                    break;
+                default:
+                    sb.append(now);
             }
         }
 
         while (!stack.isEmpty()) {
-            bw.write(stack.pop());
+            sb.append(stack.pop());
         }
-        bw.flush();
+
+        System.out.println(sb.toString());
     }
+
+    // 연산자 별 우선순위 리턴
+    public static int priority(char operator){
+
+        if(operator=='(' || operator==')'){
+            return 0;
+        } else if (operator == '+' || operator == '-') {
+            return 1;
+        } else if (operator == '*' || operator == '/') {
+            return 2;
+        }
+        return -1;
+    }
+
 }
