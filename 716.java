d1fe02915bcd0f339d9510da4237269d7ce4ea01
이주선 <96303344+dilmah0203@users.noreply.github.com>
2023-05-23 00:03:33 +0900

    Create 716.java

---
+++ 716.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public int[] findOceanViewBuildings(int[] heights) {
+        // implementation
+        int max = 0;
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = heights.length - 1; i >= 0; i++) {
+            if (heights[i] > max) {
+                list.add(i);
+                max = heights[i];
+            }
+        }
+
+        int[] result = new int[list.size()];
+        Collections.reverse(list);
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+}
