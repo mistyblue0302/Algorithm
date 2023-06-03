40a8b54ca886815686f22476e4d682c58c00f295
이주선 <96303344+dilmah0203@users.noreply.github.com>
2023-06-03 20:43:29 +0900

    Update 2667.java

--- baekjoon/DFS/2667.java
+++ baekjoon/DFS/2667.java
@@ -1,69 +1,64 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
 
-    static int n; //지도 크기
-    static int map[][];
-    static boolean visited[][];
-
-    static int count; //해당 단지에 속한 집의 수
-    static int result; //총 단지수
-    static List<Integer> list = new ArrayList<>(); //각 단지에 속하는 집의 수
-
-    static int dx[] = {0, 0, -1, 1};
-    static int dy[] = {-1, 1, 0, 0};
+    private static int n;
+    private static int dx[] = {-1, 0, 1, 0};
+    private static int dy[] = {0, 1, 0, -1};
+    private static int sum = 0; //총 단지수
+    private static int count;
+    private static int[][] array;
+    private static boolean[][] visited;
+    private static List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt(); //7
 
-        map = new int[n][n];
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); //지도의 크기
+        array = new int[n][n];
         visited = new boolean[n][n];
 
-        //입력
         for (int i = 0; i < n; i++) {
             String s = sc.next();
             for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
+                array[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
             }
         }
 
-        //탐색
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 count = 0;
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    result++;
+                if (!visited[i][j] && array[i][j] == 1) {
                     count++;
+                    sum++;
                     dfs(i, j);
                     list.add(count);
                 }
             }
         }
 
-        System.out.println(result);
+        System.out.println(sum);
         Collections.sort(list);
+
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
     }
 
-    static void dfs(int x, int y) {
+    public static void dfs(int x, int y) {
+
         visited[x][y] = true;
 
-        int nx, ny;
         for (int i = 0; i < 4; i++) {
-            nx = x + dx[i];
-            ny = y + dy[i];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                if (!visited[nx][ny] && map[nx][ny] == 1) {
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+            if (cx >= 0 && cx < n && cy >= 0 && cy < n) {
+                if (!visited[cx][cy] && array[cx][cy] == 1) {
                     count++;
-                    dfs(nx, ny);
+                    dfs(cx, cy);
                 }
             }
         }
     }
 }
+
