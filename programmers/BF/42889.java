cb6a65ea017709c644a539a6bd0bc3389be25f13
이주선 <96303344+dilmah0203@users.noreply.github.com>
2023-06-13 20:11:25 +0900

    Create 42889.java

---
+++ programmers/BF/42889.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+import java.util.*;
+
+class Solution {
+
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        Arrays.sort(stages);
+        HashMap<Integer, Double> map = new HashMap<>();
+
+        int length = stages.length;
+
+        for (int i = 1; i <= N; i++) {
+            // 실패한 사람 수 세기 위한 카운트 변수 선언
+            int cnt = 0;
+            for (int j = 0; j < stages.length; j++) {
+                if (i == stages[j]) { // 값이 같으면 카운트
+                    cnt++;
+                }
+            }
+            if (cnt == 0) { // 0은 나눌 수 없으므로 0이면 map에 0 넣음
+                map.put(i, (double) 0);
+                continue;
+            }
+            // 실패율 계산해서 map에 넣기
+            map.put(i, (double) cnt / (double) length);
+            length = length - cnt;
+        }
+        // 실패율 내림차순 정렬
+        List<Integer> keySetList = new ArrayList<Integer>(map.keySet());
+        Collections.sort(keySetList, (o1, o2) -> (map.get(o2).compareTo(map.get(o1))));
+
+        int i = 0;
+        for (Integer key : keySetList) {
+            i++;
+            answer[i - 1] = key;
+        }
+
+        return answer;
+    }
+}
