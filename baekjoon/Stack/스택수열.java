a47a9522f11aca4f77924b9bfe52b167ee8070cd
dilmah0203 <dilmah0203@naver.com>
2023-07-18 00:54:42 +0900

    2023.07.17

--- baekjoon/Stack/스택수열.java
+++ baekjoon/Stack/스택수열.java
@@ -7,7 +7,6 @@ import java.util.Stack;
 
 public class 스택수열 {
 
-    //처음 숫자 n만큼 스택에 1부터 오름차순으로 쌓다가 해당 수열의 숫자가 있으면 pop
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -18,7 +17,7 @@ public class 스택수열 {
         int start = 0;
 
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine()); //8
+            int num = Integer.parseInt(br.readLine());
 
             if (num > start) {
                 for (int j = start + 1; j <= num; j++) {
