3ac742aa63c4132b8673e9c7849a5a143c9d6710
dilmah0203 <dilmah0203@naver.com>
2023-08-11 00:52:14 +0900

    2023.08.11

--- baekjoon/BFS/말이되고픈원숭이.java
+++ baekjoon/BFS/말이되고픈원숭이.java
@@ -7,13 +7,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
 public class 말이되고픈원숭이 {
 
     static int k, w, h;
