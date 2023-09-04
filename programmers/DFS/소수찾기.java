6b378e0c7ad3f634951ae6ce865defc9788c0a7f
dilmah0203 <dilmah0203@naver.com>
2023-09-04 17:31:27 +0900

    2023.09.04

---
+++ programmers/DFS/소수찾기.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class 소수찾기 {
+    boolean[] visited = new boolean[7];
+    List<Integer> list = new ArrayList<>();
+
+    public int solution(String numbers) {
+        int answer = 0;
+
+        for(int i = 0; i < numbers.length(); i++) {
+            dfs(numbers, i + 1, "");
+        }
+
+        for(int i = 0; i < list.size(); i++) {
+            if(isPrime(list.get(i))) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public void dfs(String numbers, int length, String s) {
+        if(s.length() == length) {
+            int num = Integer.parseInt(s);
+            if(!list.contains(num)) {
+                list.add(num);
+            }
+        }
+
+        for(int i = 0; i < numbers.length(); i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                s += numbers.charAt(i);
+                dfs(numbers, length, s);
+                visited[i] = false;
+                s = s.substring(0, s.length() - 1);
+            }
+        }
+    }
+
+    public boolean isPrime(int num) {
+        if(num < 2) {
+            return false;
+        }
+
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
