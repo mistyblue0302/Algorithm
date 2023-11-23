9b759f3e589ade95ad405b78778ac497268c24d6
dilmah0203 <dilmah0203@naver.com>
2023-11-24 00:14:56 +0900

    2023.11.24

--- programmers/BF/택배배달과수거하기.java
+++ programmers/BF/택배배달과수거하기.java
@@ -6,6 +6,8 @@ class 택배배달과수거하기 {
         int delivery = 0;
         int pick = 0;
 
+        //delivery와 pick을 따로 카운팅, 끝에서부터 하나라도 0을 초과하면(물류창고로 가야하므로) -cap을 해주면
+        //물류창고 전까지 얼만큼 배달, 수거할 수 있는지 알 수 있다.
         for(int i = n - 1; i >= 0; i--) {
             delivery += deliveries[i];
             pick += pickups[i];
