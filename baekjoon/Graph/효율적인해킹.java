a37b992babf80f5ea20c685b392d0244f12410ae
dilmah0203 <dilmah0203@naver.com>
2023-12-12 21:56:21 +0900

    2023.12.12

--- baekjoon/Graph/효율적인해킹.java
+++ baekjoon/Graph/효율적인해킹.java
@@ -3,80 +3,70 @@ package Graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Queue;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class 효율적인해킹 {
-    static int N, M;
-    static ArrayList <Integer>[] arr;
-    static boolean isVisited[];
-    static int max;
-    static int cntArr[];
 
-    static void DFS(int start) {
-        isVisited[start] = true;
-        for (int i : arr[start]) {
-            if (isVisited[i]) continue;
-            cntArr[i]++; // i가 해킹할 수 있는 숫자 증가
-            DFS(i);
-        }
-    }
-
-    static void BFS(int start) {
-        Queue <Integer> que = new ArrayDeque<Integer>();
-
-        que.add(start);
-        isVisited[start] = true;
-
-        while(!que.isEmpty()) {
-            int now = que.poll();
-            for (int i : arr[now]) {
-                if (isVisited[i]) continue;
-                cntArr[i]++; // i가 해킹할 수 있는 숫자 증가
-                isVisited[i] = true;
-                que.add(i);
-            }
-        }
-    }
+    public static int n, m;
+    public static List<Integer> list[];
+    public static boolean[] visited;
+    public static int[] answer;
 
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        isVisited = new boolean[N+1];
-        cntArr = new int[N+1];
+        list = new ArrayList[n + 1];
+        answer = new int[n + 1];
 
-        // 신뢰 관계 입력
-        arr = new ArrayList[N+1];
-        for (int i=0; i<N+1; i++) arr[i] = new ArrayList <Integer>();
-        for (int i=0; i<M; i++) {
-            st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            arr[a].add(b); // a가 b를 신뢰, a는 b에게 해킹 당할 수 있음
+
+            list[b].add(a);
         }
 
-        // 1번부터 N번까지 search
-        for (int i=1; i<N+1; i++) {
-            isVisited = new boolean[N+1];
-            //DFS(i); // 메모리↓ 시간↑
-            BFS(i); // 메모리↑ 시간↓
 
+        for (int i = 1; i <= n; i++) {
+            visited = new boolean[n + 1];
+            visited[i] = true;
+            dfs(i, i);
         }
 
-        // 해킹할 수 있는 최댓값 찾기
-        for (int i=1; i<N+1; i++) {
-            if (max<cntArr[i]) max = cntArr[i];
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            max = Math.max(max, answer[i]);
         }
 
-        // 최댓값인 컴퓨터 출력
-        for (int i=1; i<N+1; i++) if (max == cntArr[i]) System.out.print(i+" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            if (answer[i] == max) {
+                sb.append(i).append(" ");
+            }
+        }
+
+        System.out.println(sb.toString());
     }
 
+    public static void dfs(int start, int now) {
+        for (int next : list[now]) {
+            if (!visited[next]) {
+                visited[next] = true;
+                dfs(start, next);
+                answer[start]++;
+            }
+        }
+    }
 }
