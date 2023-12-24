ab102c1c8f92d3abb6dd3c3a67058ec106968b1b
dilmah0203 <dilmah0203@naver,com>
2023-12-25 01:27:32 +0900

    2023.12.25

--- baekjoon/Graph/이분그래프.java
+++ baekjoon/Graph/이분그래프.java
@@ -1,5 +1,3 @@
-package Graph;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,78 +5,74 @@ import java.util.*;
 
 public class 이분그래프 {
 
-    private static List<List<Integer>> graph;
-    private static int[] colors;
-    private static final int RED = 1;
+    static int k;
+    static int v, e;
+    static List<Integer> list[];
+    static int[] visited;
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int k = Integer.parseInt(br.readLine());
-        for(int testCase = 0; testCase < k; testCase++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int V = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-            graph = new ArrayList<>();
-            colors = new int[V+1];
+        k = Integer.parseInt(br.readLine());
 
-            for(int vertex =0 ; vertex <= V; vertex++) {
-                graph.add(new ArrayList<>());
+        StringTokenizer st;
+
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            v = Integer.parseInt(st.nextToken()); //정점의 개수
+            e = Integer.parseInt(st.nextToken()); //간선의 개수
+            list = new ArrayList[v + 1];
+            visited = new int[v + 1];
+
+            //그래프 초기화
+            for (int j = 1; j <= v; j++) {
+                list[j] = new ArrayList<>();
             }
 
-            for(int edge = 0; edge < E; edge++) {
-                st = new StringTokenizer(br.readLine());
-                int from = Integer.parseInt(st.nextToken());
-                int to = Integer.parseInt(st.nextToken());
 
-                graph.get(from).add(to);
-                graph.get(to).add(from);
+            for (int j = 0; j < e; j++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                list[a].add(b);
+                list[b].add(a);
             }
 
-            boolean flag = false;
-            for(int vertex = 1; vertex <= V; vertex++) {
-                if(colors[vertex] == 0) {
-                    flag = bfs(vertex, RED);
-                }
-                if(!flag) break;
+            if (bfs()) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
             }
-            if(flag) System.out.println("YES");
-            else System.out.println("NO");
         }
-        br.close();
     }
 
-    private static boolean bfs(int start, int color) {
+    public static boolean bfs() {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
 
-        // 2. 시작 정점 임의의 색상으로 색칠
-        colors[start] = color;
+        for (int i = 1; i <= v; i++) {
+            if (visited[i] == 0) { //처음 단계에서 1번 방문처리
+                queue.add(i);
+                visited[i] = 1;
+            }
 
-        while(!queue.isEmpty()) {
-            int cur = queue.poll();
-            for(int next : graph.get(cur)) {
-                // 4. 인접 정점 색이 동일하면 이분 그래프가 아님
-                if(colors[cur] == colors[next]) return false;
+            while (!queue.isEmpty()) {
+                int now = queue.poll();
 
-                // 3. 인접 정점 색칠 안된 경우 현재 정점 반대 색깔로 색칠
-                // 색상 배열을 통해 방문 여부 확인 가능
-                if(colors[next] == 0) {
-                    colors[next] = colors[cur] * -1;
-                    queue.add(next);
+                for (int a : list[now]) {
+                    if (visited[a] == visited[now]) {
+                        return false;
+                    }
+
+                    if (visited[a] == 0) { //방문하지 않았다면
+                        queue.add(a);
+                        visited[a] = visited[now] * -1; //반대 색으로 칠하기
+                    }
                 }
             }
         }
         return true;
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 
