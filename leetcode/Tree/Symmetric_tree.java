de26b1a8ae8eef7677b1425261a30cab9fd00e48
dilmah0203 <96303344+dilmah0203@users.noreply.github.com>
2024-04-06 03:55:39 +0900

    Update Symmetric_tree.java

--- leetcode/Tree/Symmetric_tree.java
+++ leetcode/Tree/Symmetric_tree.java
@@ -4,15 +4,16 @@ class Solution {
     }
 
     public boolean isCheck(TreeNode left, TreeNode right) {
+        
+        if(left.val != right.val) { // 양쪽 값이 서로 다를 때
+            return false;
+        }
+
         if(left == null && right == null) { // 양쪽 트리의 제일 아래 노드일 때
             return true;
         }
 
         if(left == null || right == null) { // 한 쪽만 제일 아래 노드일 때
-            return false;
-        }
-
-        if(left.val != right.val) { // 양쪽 값이 서로 다를 때
             return false;
         }
 
