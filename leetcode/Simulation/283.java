b4e125a1c92deda50bc64baced08168c2bff8a98
juseon <dilmah0203@naver.com>
2024-04-06 02:02:01 +0900

    2024.04.06

---
+++ leetcode/Simulation/283.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int current = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[current] = nums[i];
+                current++;
+            }
+        }
+
+        for(int i = current; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
