ef5142470df58a1452cd263ef3edd92d2b423b48
dilmah0203 <96303344+dilmah0203@users.noreply.github.com>
2024-04-11 16:26:37 +0900

    Update Missing_Number.java

--- leetcode/Simulation/Missing_Number.java
+++ leetcode/Simulation/Missing_Number.java
@@ -2,18 +2,16 @@ import java.util.*;
 
 class Solution {
     public int missingNumber(int[] nums) {
+        int start = 0;
+        Arrays.sort(nums); 
 
-        Arrays.sort(nums); // 0 1 3
-        int n = nums[nums.length - 1]; // n : 3
-
-        int number = 0;
         for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != number) {
+            if(nums[i] != start) {
                 break;
             } else {
-                number++;
+                start++;
             }
         }
-        return number;
+        return start;
     }
 }
