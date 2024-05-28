32c35e95b744ca148de01b6c7f33f5d29d0be3ee
dilmah0203 <dilmah0203@naver.com>
2024-05-28 16:47:03 +0900

    2024.05.28

---
+++ programmers/String/이상한문자만들기.java
@@ -0,0 +1,24 @@
+package String;
+
+class 이상한문자만들기 {
+    public String solution(String s) {
+        String answer = "";
+
+        String[] str = s.split("");
+        int index = 0;
+        for(int i = 0; i < str.length; i++) {
+            if(str[i].equals(" ")) {
+                index = 0;
+            } else if(index % 2 == 0) {
+                str[i] = str[i].toUpperCase();
+                index++;
+            } else if(index % 2 != 0) {
+                str[i] = str[i].toLowerCase();
+                index++;
+            }
+            answer += str[i];
+
+        }
+        return answer;
+    }
+}
