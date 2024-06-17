d2233eef3885a0c32f5e2e81b4043c7f4d608319
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-06-18 00:15:52 +0900

    Create 두 개 뽑아서 더하기.java

---
+++ programmers/Sort/두 개 뽑아서 더하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(numbers);
+        
+        for(int i = 0; i < numbers.length - 1; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                int num = numbers[i] + numbers[j];
+                if(!list.contains(num)) {
+                    list.add(num);
+                }
+            }
+        }
+        
+        Collections.sort(list);
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer; 
+    }
+}
