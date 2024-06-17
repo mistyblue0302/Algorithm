49ee7e8c93c6fdf62caf91d962b3d47d7815b572
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-06-17 22:49:49 +0900

    Update k번째수.java

--- programmers/Sort/k번째수.java
+++ programmers/Sort/k번째수.java
@@ -2,15 +2,23 @@ package Sort;
 
 import java.util.*;
 
-class k번째수 {
+class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-
-        for (int i = 0; i < commands.length; i++) {
-            int[] temp = commands[i]; //253
-            int[] arr = Arrays.copyOfRange(array, temp[0] - 1, temp[1]);
-            Arrays.sort(arr);
-            answer[i] = arr[temp[2] - 1];
+        
+        int index = 0;
+        for(int i = 0; i < commands.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            int[] command = commands[i]; // 2 5 3
+            int a = command[0]; // 2
+            int b = command[1]; // 5
+            int c = command[2]; // 3
+            for(int j = a - 1; j <= b - 1; j++) {
+                int num = array[j];
+                list.add(array[j]);
+            }
+            Collections.sort(list);
+            answer[index++] = list.get(c - 1);
         }
         return answer;
     }
