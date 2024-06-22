97abb961f0e5a5c0958abbf67042b46bf11765c7
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-06-23 02:00:38 +0900

    Update 가장 큰 수.java

--- programmers/Sort/가장 큰 수.java
+++ programmers/Sort/가장 큰 수.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        
-        String[] array = new String[numbers.length];
-        for(int i = 0; i < array.length; i++){
-            array[i] = String.valueOf(numbers[i]);
-        }
-        //두 수를 번갈아가며 이어붙인 것을 기준으로 내림차순 비교
-        Arrays.sort(array, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-        
-        if(array[0].equals("0")) {
-            return "0"; //만약 입력으로 0,0,0이 들어온다면
+        String[] str = new String[numbers.length];
+        for(int i = 0; i < str.length; i++) {
+            str[i] = String.valueOf(numbers[i]);
         }
         
-        for(int i = 0; i < array.length; i++){
-            answer += array[i];
+        Arrays.sort(str, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        if (str[0].equals("0")) {
+            return "0";
         }
-        return answer;
+        
+        StringBuilder sb = new StringBuilder();
+        for (String s : str) {
+            sb.append(s);
+        }
+        
+        return sb.toString();
     }
 }
