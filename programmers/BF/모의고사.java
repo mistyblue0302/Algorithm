828ff6728a92c470d17598dd25133c1dc7951f34
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-08-16 00:25:35 +0900

    Update 모의고사.java

--- programmers/BF/모의고사.java
+++ programmers/BF/모의고사.java
@@ -1,31 +1,32 @@
 import java.util.*;
 
-class 모의고사 {
+class Solution {
     public int[] solution(int[] answers) {
-        int[] first = {1,2,3,4,5};
-        int[] second = {2,1,2,3,2,4,2,5};
-        int[] third = {3,3,1,1,2,2,4,4,5,5};
+        int[] array = {1,2,3,4,5};
+        int[] array2 = {2,1,2,3,2,4,2,5};
+        int[] array3 = {3,3,1,1,2,2,4,4,5,5};
         int[] count = {0,0,0};
-
+        
         for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == first[i % 5]) count[0]++;
-            if(answers[i] == second[i % 8]) count[1]++;
-            if(answers[i] == third[i % 10]) count[2]++;
+            if(array[i % 5] == answers[i]) count[0]++;
+            if(array2[i % 8] == answers[i]) count[1]++;
+            if(array3[i % 10] == answers[i]) count[2]++;
         }
-
+        
         int max = Math.max(count[0], Math.max(count[1], count[2]));
+        
         List<Integer> list = new ArrayList<>();
         for(int i = 0; i < count.length; i++) {
             if(max == count[i]) {
                 list.add(i + 1);
-            }
+            }    
         }
-
+        
         int[] answer = new int[list.size()];
-        for(int i = 0; i < answer.length; i++) {
+        for(int i=0; i < list.size(); i++){
             answer[i] = list.get(i);
         }
-        Arrays.sort(answer);
+
         return answer;
     }
 }
