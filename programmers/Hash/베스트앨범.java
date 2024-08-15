15b1a585b85aa9f12bfa32e950f3e9dad5b85707
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-08-15 18:59:25 +0900

    Update 베스트앨범.java

--- programmers/Hash/베스트앨범.java
+++ programmers/Hash/베스트앨범.java
@@ -2,45 +2,46 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         List<Integer> answer = new ArrayList<>();
         
-        for(int i = 0; i < genres.length; i++){
+        for(int i = 0; i < genres.length; i++) {
             map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
         }
         
-        //keySet를 리스트에 넣기
-        List<String> list = new ArrayList<>(map.keySet());
-        //keySet를 value 기준으로 내림차순
-        Collections.sort(list, (o1,o2) -> map.get(o2) - map.get(o1));
+        List<String> list = new ArrayList<>(map.keySet()); // 장르를 담는 리스트
+        Collections.sort(list, (o1, o2) -> map.get(o2) - map.get(o1)); // 내림차순
         
-        for(int i = 0; i < list.size(); i++){
-            String s = list.get(i); //pop
+        // 장르 별로 가장 많이 재생된 노래
+        for(int i = 0; i < list.size(); i++) {
+            String str = list.get(i); // pop
             int firstMax = 0;
-            int firstIdx = -1;
-            //첫 번째로 많이 재생된 노래 찾기
-            for(int j = 0; j < genres.length; j++){
-                if(genres[j].equals(s) && firstMax < plays[j]) {
-                    firstMax = plays[j]; //2500
-                    firstIdx = j; //4
+            int firstIndex = -1;
+            
+            for(int j = 0; j < genres.length; j++) {
+                if(str.equals(genres[j]) && firstMax < plays[j]) {
+                    firstMax = plays[j];
+                    firstIndex = j;
                 }
             }
+            
             int secondMax = 0;
-            int secondIdx = -1;
-            //두 번째로 많이 재생된 노래 찾기
+            int secondIndex = -1;
             for(int k = 0; k < genres.length; k++){
-                if(genres[k].equals(s) && secondMax < plays[k] && k != firstIdx) {
-                    secondMax = plays[k]; //600
-                    secondIdx = k; //1
+                if(str.equals(genres[k]) && secondMax < plays[k] && k != firstIndex) {
+                    secondMax = plays[k];
+                    secondIndex = k;
                 }
             }
-            answer.add(firstIdx);
-            if(secondIdx >= 0) { //두 번째로 재생된 노래가 있다면 리스트에 저장
-                answer.add(secondIdx);
+            answer.add(firstIndex);
+            
+            // 장르에 속한 곡이 하나라면, 하나의 곡만 선택
+            if(secondIndex >= 0) {
+                answer.add(secondIndex);
             }
         }
         
-        int[] array = new int[ answer.size()];
+        int[] array = new int[answer.size()];
         for(int i = 0; i < answer.size(); i++){
             array[i] = answer.get(i);
         }
