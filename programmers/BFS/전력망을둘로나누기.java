5e13bd8204d6265f903e39fc41d6481a7647f586
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-08-16 21:17:22 +0900

    Update 전력망을둘로나누기.java

--- programmers/BFS/전력망을둘로나누기.java
+++ programmers/BFS/전력망을둘로나누기.java
@@ -2,50 +2,52 @@ package BFS;
 
 import java.util.*;
 
-class 전력망을둘로나누기 {
-    static int[][] array;
-
+class Solution {
+    
+    int[][] array;
+    
     public int solution(int n, int[][] wires) {
         int answer = n;
         array = new int[n + 1][n + 1];
-
-        for (int i = 0; i < wires.length; i++) {
+        
+        for(int i = 0; i < wires.length; i++) {
             int a = wires[i][0];
             int b = wires[i][1];
             array[a][b] = 1;
             array[b][a] = 1;
         }
-
-        for (int i = 0; i < wires.length; i++) {
-            int a = wires[i][0]; //1
-            int b = wires[i][1]; //3
+        
+        for(int i = 0; i < wires.length; i++) {
+            int a = wires[i][0]; // 1
+            int b = wires[i][1]; // 3
             array[a][b] = 0;
-            array[b][a] = 0; //전력망을 하나씩 끊어보기
-            answer = Math.min(answer, bfs(n, a));
+            array[b][a] = 0; // 전력망을 하나씩 끊어보기
+            answer = Math.min(answer, bfs(a, n));
             array[a][b] = 1;
             array[b][a] = 1;
         }
         return answer;
     }
-
-    public static int bfs(int n, int first) { //9 1
+    
+    public int bfs(int node, int n) {
         boolean[] visited = new boolean[n + 1];
-        int count = 1;
-
+        int count = 1; // 전력망 개수 : 초기값 1
+        
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(first);
-        visited[first] = true;
-        while (!queue.isEmpty()) {
-            int temp = queue.poll(); //1
-            for (int i = 1; i <= n; i++) {
-                if (array[temp][i] == 1 && !visited[i]) {
+        queue.add(node);
+        visited[node] = true;
+        
+        while(!queue.isEmpty()) {
+            int temp = queue.poll(); // 1
+            for(int i = 0; i <= n; i++) {
+                if(array[temp][i] == 1 && !visited[i]) {
                     visited[i] = true;
-                    queue.offer(i);
+                    queue.add(i);
                     count++;
                 }
             }
         }
-
+        
         return (int) Math.abs(count - (n - count));
     }
 }
