3618936bef0b99e3d25c5f53b5a492cb34549c87
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-10-10 00:23:39 +0900

    Update 베스트앨범.java

--- programmers/Hash/베스트앨범.java
+++ programmers/Hash/베스트앨범.java
@@ -2,40 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> map = new HashMap<>();
         List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
         
         for(int i = 0; i < genres.length; i++) {
             map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
         }
         
-        List<String> list = new ArrayList<>(map.keySet()); // 장르를 담는 리스트
-        Collections.sort(list, (o1, o2) -> map.get(o2) - map.get(o1)); // 내림차순
-        
-        // 장르 별로 가장 많이 재생된 노래
+        List<String> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (o1, o2) -> map.get(o2) - map.get(o1));
         for(int i = 0; i < list.size(); i++) {
-            String str = list.get(i); // pop
+            String str = list.get(i); 
+            
             int firstMax = 0;
             int firstIndex = -1;
-            
             for(int j = 0; j < genres.length; j++) {
                 if(str.equals(genres[j]) && firstMax < plays[j]) {
-                    firstMax = plays[j];
+                    firstMax = plays[j]; 
                     firstIndex = j;
                 }
             }
             
+            // 두 번째 곡이 없을 수도 있음(장르에 속한 곡이 하나라면)
             int secondMax = 0;
             int secondIndex = -1;
-            for(int k = 0; k < genres.length; k++){
+            for(int k = 0; k < genres.length; k++) {
                 if(str.equals(genres[k]) && secondMax < plays[k] && k != firstIndex) {
-                    secondMax = plays[k];
-                    secondIndex = k;
+                    secondMax = plays[k]; 
+                    secondIndex = k; 
                 }
             }
-            answer.add(firstIndex);
             
-            // 장르에 속한 곡이 하나라면, 하나의 곡만 선택
+            answer.add(firstIndex);
             if(secondIndex >= 0) {
                 answer.add(secondIndex);
             }
