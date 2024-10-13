077acd4e3626c92905c6a5bbaeb0db5bc9f8be49
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-10-14 00:42:16 +0900

    Update 이중우선순위큐.java

--- programmers/Heap/이중우선순위큐.java
+++ programmers/Heap/이중우선순위큐.java
@@ -2,36 +2,37 @@ package Heap;
 
 import java.util.*;
 
-class 이중우선순위큐 {
+class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-
-        PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Collections.reverseOrder()); //내림차순
-        PriorityQueue<Integer> minQueue = new PriorityQueue<>(); //오름차순
+        int[] answer = new int[2]; 
+        PriorityQueue<Integer> minQueue = new PriorityQueue<>(); // 오름차순
+        PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Collections.reverseOrder()); // 내림차순
+        
         for(int i = 0; i < operations.length; i++) {
-            String[] s = operations[i].split(" ");
-            if(s[0].equals("I")) {
-                maxQueue.offer(Integer.parseInt(s[1]));
-                minQueue.offer(Integer.parseInt(s[1]));
-            } else if(s[0].equals("D")) {
-                if(s[1].equals("1")) {
+            String[] str = operations[i].split(" ");
+            if(str[0].equals("I")) {
+                minQueue.add(Integer.parseInt(str[1]));
+                maxQueue.add(Integer.parseInt(str[1]));
+                continue;
+            } else if(str[0].equals("D")) {
+                if(str[1].equals("1")) {
                     minQueue.remove(maxQueue.poll());
                     continue;
-                } else if(s[1].equals("-1")) {
+                } else if(str[1].equals("-1")) {
                     maxQueue.remove(minQueue.poll());
                     continue;
                 }
             }
         }
-
-        if(maxQueue.size() == 0 && minQueue.size() == 0) {
+        
+        if(minQueue.size() == 0 && maxQueue.size() == 0) {
             answer[0] = 0;
             answer[1] = 0;
+            return answer;
         } else {
             answer[0] = maxQueue.poll();
             answer[1] = minQueue.poll();
         }
-
         return answer;
     }
 }
