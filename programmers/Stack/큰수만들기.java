f7d5a607e231d24b70cf95f6297b367ac40a20f2
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-10-14 07:07:54 +0900

    Update 큰수만들기.java

--- programmers/Stack/큰수만들기.java
+++ programmers/Stack/큰수만들기.java
@@ -2,23 +2,25 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>(); // 94
         
         for(int i = 0; i < number.length(); i++) {
             char c = number.charAt(i);
             
-            while(!stack.isEmpty() && k > 0 && stack.peek() < c) {
+            while(!stack.isEmpty() && stack.peek() < c && k > 0) {
                 stack.pop();
                 k--;
             }
-            stack.push(c); 
+            stack.push(c);
         }
         
         while(k > 0) {
             stack.pop();
             k--;
         }
-
+        
+        // 스택은 후입선출 구조인데, for-each 구문으로 조회할 경우 리스트처럼 순차적으로 접근 가능
+        // 스택이 내부적으로 벡터를 상속하기 때문에 가능
         StringBuilder sb = new StringBuilder();
         for(char c : stack) {
             sb.append(c);
