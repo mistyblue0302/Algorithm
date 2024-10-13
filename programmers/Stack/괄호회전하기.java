a4c4d126601ba8dc47b7e1ad9c1f66a78568f9a3
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-10-14 03:53:13 +0900

    Update 괄호회전하기.java

--- programmers/Stack/괄호회전하기.java
+++ programmers/Stack/괄호회전하기.java
@@ -2,32 +2,32 @@ package Stack;
 
 import java.util.*;
 
-class 괄호회전하기 {
-    static public int solution(String s) {
+class Solution {
+    public int solution(String s) {
         int answer = 0;
-
-        for (int i = 0; i < s.length(); i++) {
+        
+        for(int i = 0; i < s.length(); i++) {
             Stack<Character> stack = new Stack<>();
             String str = s.substring(i, s.length()) + s.substring(0, i);
             for (int j = 0; j < str.length(); j++) {
                 char c = str.charAt(j);
                 if (stack.isEmpty()) {
                     stack.push(c);
-                } else if (c == ')' && stack.peek() == '(') {
+                } else if (stack.peek() == '(' && c == ')') { // 올바른 괄호라면 삭제하기 위해 -> 그러면 올바른 괄호로만 구성될 경우 스택의 사이즈는 0
                     stack.pop();
-                } else if (c == '}' && stack.peek() == '{') {
+                } else if (stack.peek() == '{' && c == '}') {
                     stack.pop();
-                } else if (c == ']' && stack.peek() == '[') {
+                } else if (stack.peek() == '[' && c == ']') {
                     stack.pop();
                 } else {
                     stack.push(c);
                 }
             }
-            if (stack.isEmpty()) {
+            
+            if(stack.isEmpty()) {
                 answer++;
             }
         }
-
         return answer;
     }
 }
