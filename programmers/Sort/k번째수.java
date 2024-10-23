cdf37b851d179a19d4161020bb8f06cc756be5ba
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-10-24 07:42:58 +0900

    Update k번째수.java

--- programmers/Sort/k번째수.java
+++ programmers/Sort/k번째수.java
@@ -6,19 +6,17 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        int index = 0;
         for(int i = 0; i < commands.length; i++) {
+            int[] arr = commands[i]; 
+            int a = arr[0];
+            int b = arr[1];
+            int c = arr[2];
             List<Integer> list = new ArrayList<>();
-            int[] command = commands[i]; // 2 5 3
-            int a = command[0]; // 2
-            int b = command[1]; // 5
-            int c = command[2]; // 3
             for(int j = a - 1; j <= b - 1; j++) {
-                int num = array[j];
                 list.add(array[j]);
             }
-            Collections.sort(list);
-            answer[index++] = list.get(c - 1);
+            Collections.sort(list); 
+            answer[i] = list.get(c - 1);
         }
         return answer;
     }
