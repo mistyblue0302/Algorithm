c3c2337c3c675cbe6e409959e50230350a4ce94c
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-10-25 02:27:35 +0900

    Update Hindex.java

--- programmers/Sort/Hindex.java
+++ programmers/Sort/Hindex.java
@@ -2,19 +2,20 @@ package Sort;
 
 import java.util.*;
 
-class Hindex {
+class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-
-        Arrays.sort(citations);
-        for (int i = 0; i < citations.length; i++) {
-            int h = citations.length - i;
-            if (citations[i] >= h) {
-                answer = h;
-                break;
+        Arrays.sort(citations); 
+        
+        int n = citations.length;
+        for (int i = 0; i < n; i++) {
+            int remain = n - i;  // 남은 논문 개수
+            
+            // 남은 논문 중에서 인용 횟수가 h번 이상인 경우 찾기
+            if (citations[i] >= remain) {
+                return remain;  
             }
         }
-
-        return answer;
+        
+        return 0;  // 조건을 만족하지 않는 경우
     }
 }
