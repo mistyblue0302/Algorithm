1326e526179342d4b8df9be4203e386811aab33e
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-10-27 09:28:10 +0900

    Update 방금그곡.java

--- programmers/String/방금그곡.java
+++ programmers/String/방금그곡.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public static HashMap<String, String> map = new HashMap<>();
+    Map<String, String> map = new HashMap<>();
     
     public String solution(String m, String[] musicinfos) {
         String answer = "(None)";
@@ -14,34 +14,36 @@ class Solution {
         map.put("A#", "a");
         m = change(m);
         
-        int maxTime = 0;
-        for(int i = 0; i < musicinfos.length; i++){
-            String[] str = musicinfos[i].split(",");
-            int time = getTime(str[0], str[1]); 
-            String melody = change(str[3]); 
+        int maxTime = 0; 
+        for(int i = 0; i < musicinfos.length; i++) {
+            String[] s = musicinfos[i].split(",");
+            int time = getTime(s[0], s[1]); 
+            String title = s[2]; 
+            String melody = change(s[3]); 
             
             while(melody.length() < time) {
-                melody += melody;
+                melody += melody; 
             }
             
-            melody = melody.substring(0, time);
-            if(melody.contains(m)) {
+            melody = melody.substring(0, time); 
+            
+            // 조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악 제목을 반환
+            if(melody.contains(m)) { 
                 if(maxTime < time) {
                     maxTime = time;
-                    answer = str[2];
+                    answer = title;
                 }
             }
         }
-        
         return answer;
     }
     
-    public static int getTime(String s1, String s2) {
-        String[] str1 = s1.split(":"); 
-        String[] str2 = s2.split(":");
+    public int getTime(String s1, String s2) { 
+        String[] str1 = s1.split(":");
+        String[] str2 = s2.split(":"); 
         
-        int hour1 = Integer.parseInt(str1[0]) * 60; 
-        int hour2 = Integer.parseInt(str2[0]) * 60; 
+        int hour1 = Integer.parseInt(str1[0]) * 60;
+        int hour2 = Integer.parseInt(str2[0]) * 60;
         
         int minute1 = Integer.parseInt(str1[1]);
         int minute2 = Integer.parseInt(str2[1]);
@@ -49,10 +51,10 @@ class Solution {
         return (hour2 + minute2) - (hour1 + minute1);
     }
     
-    public static String change(String str) {
+    public String change(String s) { 
         for(String key : map.keySet()) {
-            str = str.replace(key, map.get(key));
+            s = s.replace(key, map.get(key));
         }
-        return str;
+        return s;
     }
 }
