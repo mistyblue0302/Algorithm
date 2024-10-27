a401373c3b1a1dc692fd3518999bc514a9771dcf
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-10-27 16:47:08 +0900

    Update 뉴스클러스터링.java

--- programmers/Simulation/뉴스클러스터링.java
+++ programmers/Simulation/뉴스클러스터링.java
@@ -2,52 +2,57 @@ package Simulation;
 
 import java.util.*;
 
+import java.util.*;
+
 class 뉴스클러스터링 {
     public int solution(String str1, String str2) {
-        double answer = 0;
-
-        List<String> list = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();
+        double answer = 0; // 자카드 유사도는 실수라고 했기 때문에
+        
+        str1 = str1.toUpperCase(); 
+        str2 = str2.toUpperCase(); 
+        
+        List<String> list = new ArrayList<>(); 
+        List<String> list2 = new ArrayList<>(); 
+        for(int i = 0; i < str1.length() - 1; i++) {
+            char c = str1.charAt(i);
+            char c2 = str1.charAt(i +1);
+            
+            if(c >= 'A' && c <= 'Z' && c2 >= 'A' && c2 <= 'Z') {
+                list.add(c + "" + c2);
+            }
+        }
+        
+        for(int i = 0; i < str2.length() - 1; i++) {
+            char c = str2.charAt(i);
+            char c2 = str2.charAt(i +1);
+            
+            if(c >= 'A' && c <= 'Z' && c2 >= 'A' && c2 <= 'Z') {
+                list2.add(c + "" + c2);
+            }
+        }
+        
+        Collections.sort(list); 
+        Collections.sort(list2); 
+        
+        List<String> intersection = new ArrayList<>(); 
         List<String> union = new ArrayList<>();
-        List<String> intersection = new ArrayList<>();
-
-        str1 = str1.toUpperCase();
-        str2 = str2.toUpperCase();
-        for (int i = 0; i < str1.length() - 1; i++) {
-            char a = str1.charAt(i);
-            char b = str1.charAt(i + 1);
-            if (a >= 'A' && a <= 'Z' && b >= 'A' && b <= 'Z') {
-                list.add(a + "" + b);
-            }
-        }
-
-        for (int i = 0; i < str2.length() - 1; i++) {
-            char a = str2.charAt(i);
-            char b = str2.charAt(i + 1);
-            if (a >= 'A' && a <= 'Z' && b >= 'A' && b <= 'Z') {
-                list2.add(a + "" + b);
-            }
-        }
-
-        Collections.sort(list);
-        Collections.sort(list2);
-
-        for (String s : list) {
-            if (list2.remove(s)) {
+        for(String s : list) {
+            if(list2.remove(s)) {
                 intersection.add(s);
             }
+            union.add(s); 
+        }
+        
+        for(String s : list2) {
             union.add(s);
         }
-
-        for (String s : list2) {
-            union.add(s);
-        }
-
-        if (union.size() == 0) {
+        
+        if(list.size() == 0 && list2.size() == 0) { // 둘 다 공집합인 경우 
             answer = 1;
         } else {
-            answer = (double) intersection.size() / (double) union.size();
+            answer = (double) intersection.size() / union.size();
         }
+        
         return (int) (answer * 65536);
     }
 }
