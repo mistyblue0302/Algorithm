1e3c0b6c0c0b9093be3a7c65ec74e5c0681da898
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-11-16 23:53:33 +0900

    Update 숫자카드2.java

--- baekjoon/BinarySearch/숫자카드2.java
+++ baekjoon/BinarySearch/숫자카드2.java
@@ -1,11 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,20 +9,23 @@ public class Main {
             array[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(array);
+        Arrays.sort(array); // -10 -10 2 3 3 6 7 10 10 10
 
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < array.length; i++) {
+        for(int i = 0; i < array.length; i++) {
             map.put(array[i], map.getOrDefault(array[i], 0) + 1);
         }
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine(), " ");
-
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m; i++) {
-            int num = Integer.parseInt(st.nextToken()); 
-            sb.append(map.getOrDefault(num, 0)).append(' ');
+        for(int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(map.containsKey(num)) {
+                sb.append(map.get(num)).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
         }
         System.out.println(sb.toString());
     }
