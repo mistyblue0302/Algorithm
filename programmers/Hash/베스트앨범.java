d8ddfef3506264eb5e6d33de86af7f5616192c6e
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-12-05 21:52:08 +0900

    Update 베스트앨범.java

--- programmers/Hash/베스트앨범.java
+++ programmers/Hash/베스트앨범.java
@@ -3,40 +3,40 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
-        Map<String, Integer> map = new HashMap<>();
         
+        Map<String, Integer> map = new HashMap<>();
         for(int i = 0; i < genres.length; i++) {
-            map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
+            String genre = genres[i];
+            int play = plays[i];
+            map.put(genre, map.getOrDefault(genre, 0) + play);
         }
         
         List<String> list = new ArrayList<>(map.keySet());
-        Collections.sort(list, (o1, o2) -> map.get(o2) - map.get(o1));
+        Collections.sort(list, (o1, o2) -> map.get(o2) - map.get(o1)); // 내림차순
+        
         for(int i = 0; i < list.size(); i++) {
-            String str = list.get(i); 
+            String genre = list.get(i); 
             
             int firstMax = 0;
             int firstIndex = -1;
             for(int j = 0; j < genres.length; j++) {
-                if(str.equals(genres[j]) && firstMax < plays[j]) {
+                if(genre.equals(genres[j]) && firstMax < plays[j]) {
                     firstMax = plays[j]; 
                     firstIndex = j;
                 }
             }
             
-            // 두 번째 곡이 없을 수도 있음(장르에 속한 곡이 하나라면)
             int secondMax = 0;
             int secondIndex = -1;
             for(int k = 0; k < genres.length; k++) {
-                if(str.equals(genres[k]) && secondMax < plays[k] && k != firstIndex) {
+                if(genre.equals(genres[k]) && secondMax < plays[k] && k != firstIndex) {
                     secondMax = plays[k]; 
                     secondIndex = k; 
                 }
             }
             
             answer.add(firstIndex);
-            if(secondIndex >= 0) {
-                answer.add(secondIndex);
-            }
+            if(secondIndex >= 0) answer.add(secondIndex);
         }
         
         int[] array = new int[answer.size()];
