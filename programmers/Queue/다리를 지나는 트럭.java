a81e6ebab226fe71f488e5657e37b67a8493b059
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-12-10 22:24:07 +0900

    Update 다리를 지나는 트럭.java

--- programmers/Queue/다리를 지나는 트럭.java
+++ programmers/Queue/다리를 지나는 트럭.java
@@ -1,34 +1,36 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) { 
-        Queue<Integer> queue = new LinkedList<>(); //다리
-        int time = 0;
-        int sum = 0; //무게
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
         
-        for(int i = 0; i < truck_weights.length; i++){
-            int truck = truck_weights[i]; //6
+        Queue<Integer> queue = new LinkedList<>(); 
+        int sum = 0; // 다리에 올라간 트럭들의 무게
+        int time = 0; // 1
+        for(int i = 0; i < truck_weights.length; i++) {
+            int truck = truck_weights[i]; // 4
             while(true) {
-                if(queue.isEmpty()) {
+                if(queue.isEmpty()) { // 다리에 트럭이 없을 때
                     queue.add(truck);
-                    time++;
                     sum += truck;
+                    time++;
                     break;
                 } else if(queue.size() == bridge_length) {
-		    sum -= queue.poll();
-		} else {
-                    if(truck + sum > weight) {
+                    sum -= queue.poll();
+                } else {
+                    if(sum + truck > weight) {
                         queue.add(0);
                         time++;
                     } else {
                         queue.add(truck);
-                        time++;
                         sum += truck;
+                        time++;
                         break;
                     }
                 }
             }
         }
-        return time + bridge_length; //마지막 역시 다리 길이만큼 지나가야하기때문에 + 다리 길이
+        
+        return time + bridge_length;
     }
 }
