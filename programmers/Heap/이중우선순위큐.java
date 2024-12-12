ea5328a61a0e44c718f220c68123ab04becb4c48
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-12-12 23:08:26 +0900

    Update 이중우선순위큐.java

--- programmers/Heap/이중우선순위큐.java
+++ programmers/Heap/이중우선순위큐.java
@@ -4,35 +4,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2]; 
+        int[] answer = new int[2];
         PriorityQueue<Integer> minQueue = new PriorityQueue<>(); // 오름차순
         PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Collections.reverseOrder()); // 내림차순
         
         for(int i = 0; i < operations.length; i++) {
-            String[] str = operations[i].split(" ");
-            if(str[0].equals("I")) {
-                minQueue.add(Integer.parseInt(str[1]));
-                maxQueue.add(Integer.parseInt(str[1]));
-                continue;
-            } else if(str[0].equals("D")) {
-                if(str[1].equals("1")) {
-                    minQueue.remove(maxQueue.poll());
-                    continue;
-                } else if(str[1].equals("-1")) {
-                    maxQueue.remove(minQueue.poll());
-                    continue;
-                }
+            String[] s = operations[i].split(" "); // D -1
+            
+            if(s[0].equals("I")) {
+                minQueue.add(Integer.parseInt(s[1]));
+                maxQueue.add(Integer.parseInt(s[1]));
+            } else if(s[1].equals("-1")) {
+                maxQueue.remove(minQueue.poll());
+            } else if(s[1].equals("1")) {
+                minQueue.remove(maxQueue.poll());
             }
         }
         
-        if(minQueue.size() == 0 && maxQueue.size() == 0) {
+        if(maxQueue.size() == 0 && minQueue.size() == 0) {
             answer[0] = 0;
             answer[1] = 0;
-            return answer;
         } else {
             answer[0] = maxQueue.poll();
             answer[1] = minQueue.poll();
         }
+        
         return answer;
     }
 }
