19a6fbfd933d1cc7f402db7b5c8c34f1c1a2f7eb
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2024-12-13 18:24:19 +0900

    Update 가장 큰 수.java

--- programmers/Sort/가장 큰 수.java
+++ programmers/Sort/가장 큰 수.java
@@ -1,7 +1,5 @@
 import java.util.*;
 
-import java.util.*;
-
 class Solution {
     public String solution(int[] numbers) {
         String[] s = new String[numbers.length];
@@ -9,18 +7,15 @@ class Solution {
             s[i] = String.valueOf(numbers[i]);
         }
         
-        // 이어붙였을 때 큰값이 앞에 오도록 정렬됨(내림차순) 
-        // [6, 2, 10]
+        // 이어 붙였을 때 가장 큰수가 먼저 나오도록 내림차순
         Arrays.sort(s, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-        
-        // 만약 0밖에 원소가 없을 경우 "000" 등을 방지하기 위해
-        if(s[0].equals("0")) {
-            return "0";
-        }
-        
         StringBuilder sb = new StringBuilder();
         for(String str : s) {
             sb.append(str);
+        }
+        
+        if(s[0].equals("0")) {
+            return "0";
         }
         
         return sb.toString();
