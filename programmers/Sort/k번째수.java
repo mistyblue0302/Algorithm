766632c580ca78313175a312599646026942a68b
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2025-01-06 21:10:25 +0900

    Update k번째수.java

--- programmers/Sort/k번째수.java
+++ programmers/Sort/k번째수.java
@@ -4,15 +4,15 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for (int i = 0; i < commands.length; i++) {
-            int[] command = commands[i];
-            int from = command[0] - 1;
-            int to = command[1];
-            int index = command[2] - 1;
-
-            int[] copy = Arrays.copyOfRange(array, from, to);
+        for(int i = 0; i < commands.length; i++) {
+            int[] command = commands[i]; 
+            int start = command[0] - 1;
+            int end = command[1];
+            int num = command[2];
+            
+            int[] copy = Arrays.copyOfRange(array, start, end);  
             Arrays.sort(copy); 
-            answer[i] = copy[index]; 
+            answer[i] = copy[num - 1];
         }
         
         return answer;
