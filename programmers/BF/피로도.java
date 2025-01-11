2dc3fc73fdcfbe02c22b2b45ebf5a868c5e01d6e
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2025-01-12 00:54:01 +0900

    Update 피로도.java

--- programmers/BF/피로도.java
+++ programmers/BF/피로도.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
 class Solution {
+    
     int answer = 0;
     
     public int solution(int k, int[][] dungeons) {
         boolean[] visited = new boolean[dungeons.length];
-        dfs(k, dungeons, visited, 0); // 현재 피로도, 던전, 탐험할 수 있는 던전 수
+        dfs(k, dungeons, visited, 0);
         return answer;
     }
     
     public void dfs(int k, int[][] dungeons, boolean[] visited, int count) {
-        answer = Math.max(answer, count);
+        answer = Math.max(count, answer);
         
         for(int i = 0; i < dungeons.length; i++) {
-            if(!visited[i] && k >= dungeons[i][0]) { // 현재 피로도가 던전의 최소 필요 피로도보다 같거나 크다면
+            if(!visited[i] && dungeons[i][0] <= k) {
                 visited[i] = true;
                 dfs(k - dungeons[i][1], dungeons, visited, count + 1);
                 visited[i] = false;
