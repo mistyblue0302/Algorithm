1c93f921d97383dcc816df9d0990fe96f49b687c
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2025-01-11 21:08:11 +0900

    Update 소수찾기.java

--- programmers/BF/소수찾기.java
+++ programmers/BF/소수찾기.java
@@ -1,42 +1,44 @@
 import java.util.*;
 
 class Solution {
+    
     Set<Integer> set = new HashSet<>();
     
     public int solution(String numbers) {
         int answer = 0;
-        boolean[] visited = new boolean[numbers.length()];
         
+        boolean[] visited = new boolean[numbers.length()];
         dfs(numbers, "", visited);
         
-        for(Integer num : set) {
-            if(isPrime(num)) {
+        for(Integer s : set) {
+            if(isPrime(s)) {
                 answer++;
             }
         }
-        
         return answer;
     }
     
-    public void dfs(String numbers, String current, boolean[] visited) {
-        if(!current.equals("")) {
-            set.add(Integer.parseInt(current));
+    public void dfs(String numbers, String temp, boolean[] visited) {
+        if(!temp.equals("")) {
+            set.add(Integer.parseInt(temp));
         }
         
         for(int i = 0; i < numbers.length(); i++) {
             if(!visited[i]) {
                 visited[i] = true;
-                dfs(numbers, current + numbers.charAt(i), visited);
+                dfs(numbers, temp + numbers.charAt(i), visited);
                 visited[i] = false;
             }
         }
     }
     
-    public boolean isPrime(int number) {
-        if(number < 2) return false;
+    public boolean isPrime(int num) {
+        if(num < 2) {
+            return false;
+        }
         
-        for(int i = 2; i <= Math.sqrt(number); i++) {  
-            if(number % i == 0) {
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0) {
                 return false;
             }
         }
