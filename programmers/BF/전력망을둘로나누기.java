24ea3cf0d8c46aa80ee83da28cde147db614a4be
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2025-01-15 22:58:07 +0900

    Update 전력망을둘로나누기.java

--- programmers/BF/전력망을둘로나누기.java
+++ programmers/BF/전력망을둘로나누기.java
@@ -8,6 +8,7 @@ class Solution {
     
     public int solution(int n, int[][] wires) {
         int answer = n;
+        
         array = new int[n + 1][n + 1];
         
         for(int i = 0; i < wires.length; i++) {
@@ -18,32 +19,32 @@ class Solution {
         }
         
         for(int i = 0; i < wires.length; i++) {
-            int a = wires[i][0]; // 1
-            int b = wires[i][1]; // 3
-            array[a][b] = 0;
-            array[b][a] = 0; // 전력망을 하나씩 끊어보기
-            answer = Math.min(answer, bfs(a, n));
+            int a = wires[i][0]; 
+            int b = wires[i][1];
+            array[a][b] = 0; // 연결 끊기
+            array[b][a] = 0;
+            answer = Math.min(answer, bfs(n, a));
             array[a][b] = 1;
             array[b][a] = 1;
         }
         return answer;
     }
     
-    public int bfs(int node, int n) {
+    public int bfs(int n, int a) {
         boolean[] visited = new boolean[n + 1];
-        int count = 1; // 전력망 개수 : 초기값 1
+        int count = 1; // 전력망 개수
         
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(node);
-        visited[node] = true;
+        queue.add(a);
+        visited[a] = true;
         
         while(!queue.isEmpty()) {
-            int temp = queue.poll(); // 1
+            int temp = queue.poll(); 
             for(int i = 0; i <= n; i++) {
                 if(array[temp][i] == 1 && !visited[i]) {
+                    count++;
                     visited[i] = true;
                     queue.add(i);
-                    count++;
                 }
             }
         }
