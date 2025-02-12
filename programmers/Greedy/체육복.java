d522160e0c95f9ee6f00726d2d0cd456ba39cb5c
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2025-02-12 21:10:23 +0900

    Update 체육복.java

--- programmers/Greedy/체육복.java
+++ programmers/Greedy/체육복.java
@@ -1,31 +1,36 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        
         Set<Integer> lostSet = new HashSet<>();
         Set<Integer> reserveSet = new HashSet<>();
-
-        // 여벌 체육복과 도난당한 학생을 Set에 저장
-        for (int l : lost) lostSet.add(l);
-        for (int r : reserve) {
-            if (lostSet.contains(r)) {
-                // 여벌이 있지만 도난당한 학생이면 자기 체육복 사용
+        
+        // 체육복을 도난당한 학생을 Set에 저장 -> 체육복 x
+        for(int l : lost) {
+            lostSet.add(l);
+        }
+        
+        // 여벌이 있는데 도난당했으면 -> 빌려줄 수 없음
+        // 여벌이 있는데 도난당하지 않았으면 -> 빌려줄 수 있음
+        for(int r : reserve) {
+            if(lostSet.contains(r)) {
                 lostSet.remove(r);
             } else {
                 reserveSet.add(r);
             }
+        
         }
-
+        
         // 체육복 빌려주기
-        for (int r : reserveSet) {
-            if (lostSet.contains(r - 1)) {
-                lostSet.remove(r - 1); // 앞번호 학생에게 빌려줌
-            } else if (lostSet.contains(r + 1)) {
-                lostSet.remove(r + 1); // 뒷번호 학생에게 빌려줌
+        for(int r : reserveSet) {
+            if(lostSet.contains(r - 1)) {
+                lostSet.remove(r - 1);
+            } else if(lostSet.contains(r + 1)) {
+                lostSet.remove(r + 1);
             }
         }
-
-        return n - lostSet.size(); // 전체 학생 수 - 체육복 없는 학생 수
+        return n - lostSet.size();
     }
 }
