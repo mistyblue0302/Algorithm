778e8f37b7537f4d0037984b8a5a02982af12a8e
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2025-03-12 20:23:32 +0900

    Create 디스크 컨트롤러.java

---
+++ programmers/Queue/디스크 컨트롤러.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        
+        // 작업 시작 시간이 빠른것부터 
+        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
+        // 시작시간이 같다면 작업시간이 짧은 것부터
+        PriorityQueue<int[]> queue = new PriorityQueue<>(
+            (o1, o2) -> o1[1] - o2[1]
+        );
+        
+        int time = 0; // 현재 시간 : 17
+        int sum = 0; // 총 반환시간 : 3 + 5 + 16
+        int index = 0; // jobs 인덱스 : 2
+        int completedJobs = 0; // 완료된 작업 수 : 3
+        while(completedJobs < jobs.length) {
+            while(index < jobs.length && jobs[index][0] <= time) {
+                queue.add(jobs[index]);
+                index++;
+            }
+            
+            if(queue.isEmpty()) {
+                time = jobs[index][0];
+            } else {
+                int[] current = queue.poll();
+                time += current[1]; 
+                sum += time - current[0];
+                completedJobs++;
+            }
+        }
+        return sum / jobs.length;
+    }
+}
