275ca9e78e9079ee16f8700ced6902ce2af2f654
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2025-03-13 18:05:33 +0900

    Update 디스크 컨트롤러.java

--- programmers/Queue/디스크 컨트롤러.java
+++ programmers/Queue/디스크 컨트롤러.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] jobs) {
-        int answer = 0;
+    public int solution(int[][] jobs) { // 요청 시간, 소요 시간을 담은 배열
+        int answer = 0; // 총 반환시간
         
-        // 작업 시작 시간이 빠른것부터 
+        // 시작시간이 빠른 것 -> 작업 소요시간이 짧은 것 -> 작업 번호가 작은 것
         Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
-        // 시작시간이 같다면 작업시간이 짧은 것부터
-        PriorityQueue<int[]> queue = new PriorityQueue<>(
-            (o1, o2) -> o1[1] - o2[1]
-        );
         
-        int time = 0; // 현재 시간 : 17
-        int sum = 0; // 총 반환시간 : 3 + 5 + 16
-        int index = 0; // jobs 인덱스 : 2
-        int completedJobs = 0; // 완료된 작업 수 : 3
+        // 시작시간이 같다면 작업 소요시간이 짧은 것
+        PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]); // [1 9] [ 3, 5]
+        
+        int completedJobs = 0; // 완료된 작업의 수
+        int time = 0; // 현재 시간
+        int index = 0; 
+        
         while(completedJobs < jobs.length) {
-            while(index < jobs.length && jobs[index][0] <= time) {
+            // 현재시간에서 실행가능한 작업을 모두 큐에 넣는다.
+            while (index < jobs.length && jobs[index][0] <= time) {
                 queue.add(jobs[index]);
                 index++;
             }
@@ -25,11 +25,11 @@ class Solution {
                 time = jobs[index][0];
             } else {
                 int[] current = queue.poll();
-                time += current[1]; 
-                sum += time - current[0];
+                time += current[1];
+                answer += time - current[0];
                 completedJobs++;
             }
         }
-        return sum / jobs.length;
+        return answer / jobs.length;
     }
 }
