e1a73184df2add23a7821c5b7dc8d3dff7874d3d
mistyblue0302 <96303344+mistyblue0302@users.noreply.github.com>
2025-04-09 17:13:54 +0900

    Create 올바른 괄호.java

---
+++ programmers/Stack/올바른 괄호.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if(stack.isEmpty()) {
+                stack.push(c);
+            } else if(c == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else if(c == '(') {
+                stack.push(c);
+            }
+        }
+        
+        if(stack.isEmpty()) {
+            return answer;
+        } else {
+            answer = false;
+            return answer;
+        }
+    }
+}
